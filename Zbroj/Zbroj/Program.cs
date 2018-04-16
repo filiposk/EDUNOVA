1136c37f98d15f154e3fb78ff047946f914d6f27
Filip <fpavlovic1995@gmail.com>
2018-04-16 23:30:10 +0200

    - added conosle application

---
+++ Zbroj/Zbroj/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zbroj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isRunning = true;
+            while (isRunning)
+            {
+                int op1, op2;
+                Console.Write("Enter first operand: ");
+                op1 = int.Parse(Console.ReadLine());
+                Console.Write("Enter second operand: ");
+                op2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(String.Format("Result is: {0}", op1 + op2));
+                Console.WriteLine("---------------------------");
+            }
+        }
+    }
+}
